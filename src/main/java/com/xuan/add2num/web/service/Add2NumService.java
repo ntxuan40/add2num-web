19a87ba79e2dbac8d88e7b3ba646ea44fb75664e
tinvietsoft <ntxuan2401@gmail.com>
2026-09-18 23:15:47 +0700

    add submodule

--- src/main/java/com/xuan/add2num/web/service/Add2NumService.java
+++ src/main/java/com/xuan/add2num/web/service/Add2NumService.java
@@ -1,5 +1,6 @@
 package com.xuan.add2num.web.service;
 
+import com.xuan.add2num.MyBigNumber;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,8 +8,8 @@ public class Add2NumService {
 
     public String add(String stn1, String stn2) {
 
-        Add2Num calculator = new Add2Num();
+        MyBigNumber calculator = new MyBigNumber();
 
-        return calculator.add(stn1, stn2);
+        return calculator.sum(stn1, stn2);
     }
 }
